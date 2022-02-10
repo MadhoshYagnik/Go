9a65b79483fe006a9f0dc8fb9263f36f6816f58b
Madhosh Yagnik <madhosh1yagnik@gmail.com>
2022-02-10 11:17:23 +0530

    client server config finally working

--- Go/main.go
+++ Go/main.go
@@ -80,3 +80,40 @@
 // 		fmt.Println(element.string, " => ", element.int)
 // 	}
 // }
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", processor)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
+func processor(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	text := r.FormValue("text")
+	d := struct {
+		Text string
+	}{
+		Text: text,
+	}
+	tpl.ExecuteTemplate(w, "processor.gohtml", d)
+
+	// fmt.Println("hello ", text)
+}
