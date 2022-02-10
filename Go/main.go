577024e10865d6750fcd2745ac7ccf40fe2cad56
Madhosh Yagnik <madhosh1yagnik@gmail.com>
2022-02-10 11:52:18 +0530

    Finalized the client-server implementation
    output of program will be only visible in bash, however user inputs the paragraph from html form

--- Go/main.go
+++ Go/main.go
@@ -83,20 +83,23 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/bbalet/stopwords"
 )
 
 var tpl *template.Template
+var words string
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
-}
-
-func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/process", processor)
-	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -114,6 +117,99 @@ func processor(w http.ResponseWriter, r *http.Request) {
 		Text: text,
 	}
 	tpl.ExecuteTemplate(w, "processor.gohtml", d)
+	words = text
+
+	out := topten(words)
+	fmt.Println(out)
+	outWithRemoval := toptenusestopwords(words)
+	fmt.Println(outWithRemoval)
 
 	// fmt.Println("hello ", text)
+}
+
+// word counter
+
+func topten(words string) []string {
+	input := strings.Fields(words)
+	count := make(map[string]int)
+
+	for _, word := range input {
+		reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+		if err != nil {
+			log.Fatal(err)
+		}
+		word := reg.ReplaceAllString(word, "")
+		_, match := count[word]
+		if match {
+			count[word] += 1
+		} else {
+			count[word] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(count))
+	out := make([]string, 0)
+	for k := range count {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return count[keys[i]] > count[keys[j]]
+	})
+
+	for i, name := range keys {
+		var s = strconv.Itoa(count[name])
+		if i != 10 {
+			out = append(out, name+":"+s)
+		} else {
+			break
+		}
+	}
+	return out
+}
+
+func toptenusestopwords(words string) []string {
+	cleanContent := stopwords.CleanString(words, "en", true)
+
+	input := strings.Fields(cleanContent)
+	count := make(map[string]int)
+
+	for _, word := range input {
+		reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+		if err != nil {
+			log.Fatal(err)
+		}
+		word := reg.ReplaceAllString(word, "")
+		_, match := count[word]
+		if match {
+			count[word] += 1
+		} else {
+			count[word] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(count))
+	out := make([]string, 0)
+	for k := range count {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		return count[keys[i]] > count[keys[j]]
+	})
+
+	for i, name := range keys {
+		var s = strconv.Itoa(count[name])
+		if i != 10 {
+			out = append(out, name+":"+s)
+		} else {
+			break
+		}
+	}
+	return out
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/process", processor)
+	http.ListenAndServe(":8080", nil)
+
 }
